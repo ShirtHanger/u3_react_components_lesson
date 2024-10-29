a9dc792e2209c872b48e3642a0d27756945fe3da
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-29 13:31:27 -0500

    Checkpoint

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -5,32 +5,80 @@ import Navbar from './components/Navbar/Navbar'
 import FounderListItem from './components/FounderListItem/FounderListItem'
 
 const App = () => {
+
+  const providenceAssets = [
+    {
+        name: "Viktor Novikov",
+        title: "Oligarch",
+        location: "France",
+        coverCompany: "Sanguine",
+        description: `A self-made billionaire, Viktor Novikov prospered in the post-USSR boom of capitalism, making his early fortunes in real estate. When he took a nasty financial beating during the recession, Novikov turned to more creative sources of income: corporate espionage, extortion, insurance fraud, kidnapping and arson. Anything to maintain his ostentatious way of life.`,
+        id: 1
+    },
+    {
+        name: "Silvio Caruso",
+        title: "Doctor",
+        location: "Italy",
+        coverCompany: "Ether",
+        description: `Arguably one of the world's finest stem cell researchers, Silvio Caruso is, however, a troubled genius: neurotic, angst-ridden and intensely socially awkward.`,
+        id: 2
+    },
+    {
+        name: "Thomas Cross",
+        title: "Multi-Billionaire Media Mogul",
+        location: "United States",
+        coverCompany: "Cross Holdings",
+        description: `Thomas Cross was a multi-billionaire media mogul and CEO for conglomerate Cross Holdings, in addition to being a secret operative for Providence. He was the father of acclaimed rock musician, Jordan Cross.`,
+        id: 3
+    },
+    {
+      name: "Yuki Yamazaki",
+      title: "Lawyer",
+      location: "Japan",
+      coverCompany: "Yakuza",
+      description: `Yuki Yamazaki is a cutthroat Tokyo trial lawyer and a secret operative of Providence.`,
+      id: 4
+    },
+    {
+      name: "Erich Soders",
+      title: "Retired ICA Operative",
+      location: "Sweden",
+      coverCompany: "International Contract Agency",
+      description: `Retired from the field in 1981, Erich Soders is still considered the best ICA agent of his generation.`,
+      id: 5
+    },
+    {
+      name: "Claus Hugo Strandberg",
+      title: "Banker",
+      location: "Morocco",
+      coverCompany: "Milton-Fitzpatrick",
+      description: `Claus Strandberg, a Swedish citizen, is the former CEO of Morocco's largest private bank. Accused of billion-dollar investment fraud against the Moroccan people, he is currently under siege inside the Swedish Consulate in Marrakesh.`,
+      id: 6
+    }
+  ];
+
+
+
   return (
     <> 
       <Navbar />
       <main>
-        <h1>Welcome to Hyperbo.ly</h1>
-        <h2>The future is firmly in front of us!</h2>
-        <p>If you can't explain it to a fifth-grader, our product does it.</p>
+        <h1>Providence Assets</h1>
+        <h2>[Exposed to the public by the Freedom Fighters]</h2>
+        <p>List of Oligarchs, CEOs, and politicians associeted with Providence</p>
       </main>
-      <FounderListItem 
-      name="Viktor Novikov"
-      title="CEO"
-      company="Sanguine" />
-      <h2>Our Founders</h2>
-      <ul>
-        <li>
-          <h3>Elizabeth Holmes: CEO</h3>
-          <p>MBA from SuperLegit University</p>
-        </li>
-        <li>
-          <h3>Sam Bankman-Fried: CFO</h3>
-          <p>CPA from TotallyReal State</p>
-        </li>
-        <li>
-          <h3>Matt Damon: CMO</h3>
-          <p>Was in that Movie You saw</p>
-        </li>
+      <h2>[DECLASSIFIED]</h2>
+      <ul> {/* Use array.map to populate ALL data from array of objects easily */}
+      {providenceAssets.map((providenceAsset) => (
+          <FounderListItem
+            key={providenceAsset.id}
+            providenceAsset={ providenceAsset }
+            /* name={providenceAsset.name}
+            title={providenceAsset.title}
+            location={providenceAsset.location}
+            coverCompany={providenceAsset.coverCompany} */
+          />
+        ))}
       </ul>
     </>
   )
--- react-components/src/components/FounderListItem/FounderListItem.jsx
+++ react-components/src/components/FounderListItem/FounderListItem.jsx
@@ -1,11 +1,20 @@
 import './FounderListItem.css'
 /* Anything in brackets renders JavaScript */
 /* props is an object saved in react, shorten everything */
+/* You can use props without curly braces, or { parameterName } */
+/* With props, you must pass in every variable with props.variable or props.object.variable */
 
 const FounderListItem = (props) => { /* Props can be used to pass in data like arguements, JavaScript stuff! */
+
+    const { providenceAsset } = props
+
     return (
         <li>
-        <h3>Hello, my name is {props.name}, the {props.title} of {props.company}</h3>
+        <h3><strong>Name:</strong> {providenceAsset.name}</h3>
+        <p>{providenceAsset.description}</p>
+        <h4><strong>Role:</strong> {providenceAsset.title}</h4>
+        <h4><strong>Cover Organization:</strong> {providenceAsset.coverCompany}</h4>
+        <h4><strong>Home Base:</strong> {providenceAsset.location}</h4>
         </li>
     )
 }
