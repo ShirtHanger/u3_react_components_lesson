a4e9c0eccc73ffc9b6b43b5f4ebfdc0052d2117e
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-29 12:16:16 -0500

    Checkpoint

---
+++ react-components/src/App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <> 
+      <Navbar />
+      <main>
+        <h1>Welcome to Hyperbo.ly</h1>
+        <h2>The future is firmly in front of us!</h2>
+        <p>If you can't explain it to a fifth-grader, our product does it.</p>
+      </main>
+    </>
+  )
+}
+
+export default App
